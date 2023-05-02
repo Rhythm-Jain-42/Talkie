114f8ae478d814d95087302e53936027cf9a232f
Rhythm-Jain-42 <rhythmjain101@gmail.com>
2023-05-02 21:01:06 +0530

    nextCommit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { auth } from '../firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import SignIn from './SignIn'
+
+const style = {
+  nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+  heading: `text-white text-3xl`
+}
+
+const Navbar = () => {
+  const [user] = useAuthState(auth)
+  console.log(user)
+  return (
+    <div className={style.nav}>
+      <h1 className={style.heading}>Chat App</h1>
+      <SignIn />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPbZtIoQ9xdxn5SQTQX7H5B7dSSgkBaDU",
+  authDomain: "talkie-c19bd.firebaseapp.com",
+  projectId: "talkie-c19bd",
+  storageBucket: "talkie-c19bd.appspot.com",
+  messagingSenderId: "322363739098",
+  appId: "1:322363739098:web:d775abd3256bad0e9e8ee3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
