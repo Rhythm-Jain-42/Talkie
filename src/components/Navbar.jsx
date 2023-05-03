23224fd3118230b7e9d34f14f97370787776042a
Rhythm-Jain-42 <rhythmjain101@gmail.com>
2023-05-03 23:58:38 +0530

    talkie commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { auth } from '../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import SignIn from './SignIn'
+import LogOut from './LogOut'
 
 const style = {
   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -14,7 +15,7 @@ const Navbar = () => {
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>Chat App</h1>
-      <SignIn />
+      {user ? <LogOut /> : <SignIn />}
     </div>
   )
 }
