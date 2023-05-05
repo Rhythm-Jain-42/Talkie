e9e7970a5b44eff1dfc5581f41e25f1fc078889e
Rhythm-Jain-42 <rhythmjain101@gmail.com>
2023-05-05 16:23:53 +0530

    talkie commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 
 import { auth } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Chat from './components/Chat';
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
@@ -16,6 +17,7 @@ function App  ()  {
     <div className={style.appContainer}>
       <section className={style.sectionContainer}>
         <Navbar/>
+        {user ? <Chat /> : null}
       </section>
     </div>
   )
---
+++ src/components/Chat.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect, useRef} from "react";
+import Message from "./Message";
+import { db } from '../firebase';
+import { query, collection, orderBy, onSnapshot } from 'firebase/firestore';
+import SendMessage from "./SendMessage";
+
+const style = {
+  main: `flex flex-col p-[10px]`,
+};
+
+const Chat = () => {
+
+    const [Messages, setMessages] = useState([]);
+    const scroll = useRef();
+
+    useEffect(() => {
+        const q = query(collection(db, 'Messages'), orderBy('timestamp'));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+          let Messages = [];
+          querySnapshot.forEach((doc) => {
+            Messages.push({ ...doc.data(), id: doc.id });
+          });
+          setMessages(Messages);
+        });
+        return () => unsubscribe();
+      }, []);
+
+  return (
+    <>
+      <main className={style.main}>
+      {Messages &&
+          Messages.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+      </main>
+      {/* send msg component */}
+      <SendMessage scroll={scroll}/>
+      <span ref={scroll}></span>
+    </>
+  );
+};
+
+export default Chat;
